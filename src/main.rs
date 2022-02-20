fe841b239ce625bc38737edea860fe3c24220b6c
PepperLola <julian.orion.wright@gmail.com>
2022-02-19 23:23:21 -0800

    fully functional now with controls and tui?

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::fs::File;
 use std::io;
+use std::cmp;
 use std::time;
 use std::thread;
 use std::io::prelude::*;
+use std::collections::HashMap;
 use std::path::Path;
+use std::iter::Iterator;
+use std::iter::FromIterator;
 use termion::{clear, cursor, color, style};
 use termion::raw::IntoRawMode;
 use termion::input::TermRead;
@@ -14,14 +18,40 @@ fn string_index(s: String, i: usize) -> char {
     return b as char;
 }
 
+fn next_type(t: LetterType) -> LetterType {
+    match t {
+        LetterType::INCORRECT => return LetterType::WRONG_POSITION,
+        LetterType::WRONG_POSITION => return LetterType::CORRECT,
+        LetterType::CORRECT => return LetterType::INCORRECT,
+    }
+}
+
+fn previous_type(t: LetterType) -> LetterType {
+    match t {
+        LetterType::INCORRECT => return LetterType::CORRECT,
+        LetterType::WRONG_POSITION => return LetterType::INCORRECT,
+        LetterType::CORRECT => return LetterType::WRONG_POSITION,
+    }
+}
+
+fn type_color_string(t: LetterType) -> String {
+    match t {
+        LetterType::INCORRECT => return String::from("\x1b[90m"),
+        LetterType::WRONG_POSITION => return String::from("\x1b[33m"),
+        LetterType::CORRECT => return String::from("\x1b[32;1m"),
+    }
+}
+
+#[derive(Copy, Clone)]
 enum LetterType {
     INCORRECT,
     WRONG_POSITION,
-    CORRECT
+    CORRECT,
 }
 
+#[derive(Copy, Clone)]
 struct Letter {
-    character: String,
+    character: char,
     letterType: LetterType,
 }
 
@@ -45,7 +75,10 @@ fn main() {
 
             words.retain(|&word| word.chars().count() == 5);
 
-            let mut index: i32 = 0;
+            let mut known_correct: HashMap<u16, char> = HashMap::new();
+
+            let mut index: usize = 1;
+            let mut line: u16 = 1;
 
             let mut stdout = io::stdout().into_raw_mode().unwrap();
             let mut stdin = termion::async_stdin().keys();
@@ -55,7 +88,10 @@ fn main() {
 
             stdout.flush().unwrap();
 
-            let mut word: Vec<&char> = Vec::from(['', '', '', '', '']);
+            let mut word: Vec<Letter> = Vec::new();
+            for _ in 0..5 {
+                word.push(Letter { character: 'a', letterType: LetterType::INCORRECT });
+            }
 
             loop {
                 let b = stdin.next();
@@ -68,7 +104,7 @@ fn main() {
                             while index < 1 {
                                 index += 5;
                             }
-                            write!(stdout, "{}", cursor::Goto(index as u16, 1)).unwrap();
+                            write!(stdout, "{}", cursor::Goto(index as u16, line)).unwrap();
                             stdout.lock().flush().unwrap();
                         },
                         Key::Right => {
@@ -77,26 +113,77 @@ fn main() {
                             if index < 1 {
                                 index = 1;
                             }
-                            write!(stdout, "{}", cursor::Goto(index as u16, 1)).unwrap();
+                            write!(stdout, "{}", cursor::Goto(index as u16, line)).unwrap();
                             stdout.lock().flush().unwrap();
+                        },
+                        Key::Up => {
+                            word[index - 1].letterType = previous_type(word[index - 1].letterType);
+                            write!(stdout, "{}{}{}", cursor::Goto(index as u16, line), type_color_string(word[index - 1].letterType), word[index - 1].character).unwrap();
+                        },
+                        Key::Down => {
+                            word[index - 1].letterType = next_type(word[index - 1].letterType);
+                            write!(stdout, "{}{}{}", cursor::Goto(index as u16, line), type_color_string(word[index - 1].letterType), word[index - 1].character).unwrap();
                         },
                         Key::Ctrl('c') => {
                             break;
                         },
+                        Char('\n') => {
+                            line += 1;
+                            for i in 0..5 {
+                                let letter: Letter = word[i];
+                                match letter.letterType {
+                                    LetterType::INCORRECT => words.retain(|&word| !word.contains(letter.character)),
+                                    LetterType::WRONG_POSITION => words.retain(|&word| string_index(word.to_string(), i) != letter.character),
+                                    LetterType::CORRECT => {
+                                        words.retain(|&word| string_index(word.to_string(), i) == letter.character);
+                                        known_correct.insert(i as u16, letter.character);
+                                    },
+                                }
+                            }
+                            let length = cmp::min(10, words.len());
+                            for i in 0..length {
+                                write!(stdout, "{}        ", cursor::Goto(20, (i + 1) as u16)).unwrap();
+                                write!(stdout, "{}{}.{}", cursor::Goto(20, (i + 1) as u16), (i + 1).to_string(), words[i]).unwrap();
+                            }
+                            for i in 0..(10 - length) {
+                                write!(stdout, "{}        ", cursor::Goto(20, (i + length + 1) as u16)).unwrap();
+                            }
+                            write!(stdout, "{}", cursor::Goto(index as u16, line)).unwrap();
+                            stdout.lock().flush().unwrap();
+                            for i in 0..5 {
+                                word[i].letterType = LetterType::INCORRECT;
+                            }
+                        },
                         Char(c) => {
-                            write!(stdout, "{}", c).unwrap();
+                            word[index - 1].character = c;
+                            let idx = &((index - 1) as u16);
+                            if known_correct.contains_key(idx) {
+                                match known_correct.get(idx) {
+                                    Some(other_c) => {
+                                        if other_c == &c {
+                                            word[index - 1].letterType = LetterType::CORRECT;
+                                        }
+                                    },
+                                    _ => {},
+                                }
+                            } else {
+                                word[index - 1].letterType = LetterType::INCORRECT;
+                            }
+                            write!(stdout, "{}", cursor::Goto(index as u16, line)).unwrap();
+                            write!(stdout, "{}{}", type_color_string(word[index - 1].letterType), c).unwrap();
                             index += 1;
                             index %= 6;
                             if index < 1 {
                                 index = 1;
                             }
-                            word[(index as usize) - 1] = &c;
-                            write!(stdout, "{}", cursor::Goto(index as u16, 1)).unwrap();
+                            write!(stdout, "{}", cursor::Goto(index as u16, line)).unwrap();
                             stdout.lock().flush().unwrap();
                         },
                         _ => {},
                     }
+                    write!(stdout, "\x1b[0m").unwrap();
                 }
+
 
                 thread::sleep(time::Duration::from_millis(50));
             }
